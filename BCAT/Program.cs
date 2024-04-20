cadefe802196a73bf0b83b27f9ece5167e877773
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-04-21 01:46:01 +0200

    Deleted bad comments.

--- BCAT/Program.cs
+++ BCAT/Program.cs
@@ -23,8 +23,7 @@ public class Program
         blockchain.chain[1].prevHash = "123";        
         Console.WriteLine(BlockchainValidator.ValidateBlockchain(blockchain) ? "BLOCKCHAIN IS VALID" : "BLOCKCHAIN IS NOT VALID");
         // END OF VERY BIG TEST
-
-
+        
     }
 
     static public void InitTest(Blockchain blockchain)
@@ -50,39 +49,6 @@ public class Program
         Console.WriteLine("Blockchain wallets after transactions:");
         blockchain.wallets.ForEach(wallet => Console.WriteLine(wallet.SerializerToJsonString()));
         
-
-        // Console.WriteLine("Creating Transaction 1...");
-        // Transaction transaction1 = new Transaction();
-        // transaction1.CreateTransaction("Alice_public", "Bob_public", 100, "PrivateKey_alice");
-        //
-        // (_, string err) = blockchain.CreateBlock(transaction1);
-        // if (err != "") 
-        // {
-        //     Console.WriteLine(err);
-        //     return;
-        // }
-        //
-        // string jsonBlock = blockchain.chain[0].SerializerToJsonString();
-        //
-        //
-        // Console.WriteLine(jsonBlock);
-        //
-        // Console.WriteLine("Creating Transaction 2...");
-        // Transaction transaction2 = new Transaction();
-        // transaction2.CreateTransaction("Bob_public", "Alice_public", 200, "PrivateKey_bob");
-        //
-        // (_, err) = blockchain.CreateBlock(transaction2);
-        // if (err != "") 
-        // {
-        //     Console.WriteLine(err);
-        //     return;
-        // }
-        //
-        // jsonBlock = blockchain.chain[1].SerializerToJsonString();
-        //
-        //
-        // Console.WriteLine(jsonBlock + "\n");
-
     }
 }
 
