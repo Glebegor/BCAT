5ebd73f4425eaa64e1669c13b5fae4220f346fe0
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-04-22 14:37:31 +0200

    Possibled much nodes on one host.

--- BCAT/Entities/Commons/Clients/Server.cs
+++ BCAT/Entities/Commons/Clients/Server.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Net.Sockets;
 
 namespace BCAT.Entities.Commons.Clients;
 
@@ -6,21 +7,44 @@ public class Server
 {
     public HttpListener httpListener;
     public string host;
-    public string port;
+    public int port;
 
     public Server()
     {
         this.httpListener = new HttpListener();
         this.host = "localhost";
-        this.port = "8080";
-        
-        httpListener.Prefixes.Add("http://" + host + ":" + port + "/");
+        this.port = 8080;
+        while (true)
+        {
+            if (!CheckPort(port))
+            {
+                httpListener.Prefixes.Add("http://" + host + ":" + port.ToString() + "/");
+                break; 
+            }
+            else
+            {
+                port += 1;
+            }
+        }
     }
-    
+
+    public bool CheckPort(int port)
+    {
+        try
+        {
+            TcpClient tcpClient = new TcpClient();
+            tcpClient.Connect("localhost", port);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
     public void Start()
     {
         httpListener.Start();
-        Console.WriteLine("Started server on http://" + host + ":" + port + "/");
+        Console.WriteLine("Started server on http://" + host + ":" + port.ToString() + "/");
         ThreadPool.QueueUserWorkItem((o) =>
         {
             while (httpListener.IsListening)
