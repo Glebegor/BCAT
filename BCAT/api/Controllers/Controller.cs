a0ce404a33685d171faf2d1e305fc8f40461dd9f
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-04-23 10:00:42 +0200

    Created requests, responses.

--- BCAT/api/Controllers/Controller.cs
+++ BCAT/api/Controllers/Controller.cs
@@ -11,6 +11,11 @@ public class NodeController : Controller
 {
     public override void HandelRequest(HttpListenerContext context)
     {
+        // Get the request object
+        HttpListenerRequest request = context.Request;
+
+        // Get the response object
+        HttpListenerResponse response = context.Response;
     }
 }
 
@@ -18,6 +23,11 @@ public class NodeMiningController : Controller
 {
     public override void HandelRequest(HttpListenerContext context)
     {
+        // Get the request object
+        HttpListenerRequest request = context.Request;
+
+        // Get the response object
+        HttpListenerResponse response = context.Response;
     }
 }
 
@@ -25,5 +35,10 @@ public class MinerController : Controller
 {
     public override void HandelRequest(HttpListenerContext context)
     {
+        // Get the request object
+        HttpListenerRequest request = context.Request;
+
+        // Get the response object
+        HttpListenerResponse response = context.Response;
     }
 }
