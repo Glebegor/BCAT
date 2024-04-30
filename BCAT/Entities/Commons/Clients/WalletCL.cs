486b82a9fc0b78ffed073e78df24039c8596e459
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-04-30 22:53:16 +0200

    Working with wallets.

--- BCAT/Entities/Commons/Clients/WalletCL.cs
+++ BCAT/Entities/Commons/Clients/WalletCL.cs
@@ -2,7 +2,32 @@ namespace BCAT.Entities.Commons.Clients;
 
 public class WalletCL : Client
 {
+    Wallet wallet;
     public override void Run()
+    {
+        Console.WriteLine("1. Create wallet");
+        Console.WriteLine("2. Load wallet");
+        int choise = int.Parse(Console.ReadLine());
+        switch (choise)
+        {
+            case 1:
+                CreateWallet();
+                break;
+            case 2:
+                LoadWallet();
+                break;
+            default:
+                Console.WriteLine("Invalid choise");
+                break;
+        }
+    }
+
+    public void CreateWallet()
+    {
+        
+    }
+
+    public void LoadWallet()
     {
         
     }
